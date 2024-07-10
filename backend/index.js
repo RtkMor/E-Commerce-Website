07909fb86dfdee9ea7aa5e783c72098d868a4294
RtkMor <rtkmor@gmail.com>
2024-07-10 19:25:48 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     methods: ["POST", "GET"],
     credentials: true,
   })
